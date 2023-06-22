4506b0f55267646a9150af3a676a95bf2b6e1bef
siddique <siddique3497@gmail.om>
2023-06-23 00:53:26 +0500

    re-fixed DB_HOST from environment var

--- core/settings.py
+++ core/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 DB_ENGINE   = os.getenv('DB_ENGINE'   , None)
 DB_USERNAME = os.getenv('DB_USERNAME' , None)
 DB_PASS     = os.getenv('DB_PASS'     , None)
-DB_HOST     = os.getenv('DATABASE_URL'     , None)
+DB_HOST     = os.getenv('DB_HOST'     , None)
 DB_PORT     = os.getenv('DB_PORT'     , None)
 DB_NAME     = os.getenv('DB_NAME'     , None)
 
