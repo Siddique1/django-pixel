74d96fabdb86293d265e158ba95e076135803d64
siddique <siddique3497@gmail.om>
2023-06-23 21:58:18 +0500

    remove an old stuff useless

--- core/settings.py
+++ core/settings.py
@@ -140,16 +140,16 @@ DATABASES = {
     )
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_at_render',
-        'USER': 'afridi3497',
-        'PASSWORD': 'mypassword',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'db_at_render',
+#         'USER': 'afridi3497',
+#         'PASSWORD': 'mypassword',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
